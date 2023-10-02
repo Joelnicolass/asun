1552b965711b26ff6200be39e0896c08e730f252
Albano <albanokainer@hotmail.com>
2023-10-02 00:24:54 -0300

    Update view

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -1,15 +1,18 @@
-import { Locale } from "@/i18n.config"
-import { getDictionary } from "@/lib/dictionary"
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '@/lib/dictionary';
 
 const Dashboard = async ({
-  params: { lang }
+  params: { lang },
 }: {
-  params: { lang: Locale }
-}) =>{
-  const { page } = await getDictionary(lang)
+  params: { lang: Locale };
+}) => {
+  const { page } = await getDictionary(lang);
   return (
-    <div>{page.home.description}</div>
-  )
-}
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-3xl font-bold'>{page.about.title}</h1>
+      <h2 className='text-3xl font-bold'>{page.about.description}</h2>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
