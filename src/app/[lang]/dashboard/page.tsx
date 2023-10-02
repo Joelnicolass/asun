931cea918b235113b69e98c584bbdd07782e4714
Albano <albanokainer@hotmail.com>
2023-10-02 16:03:16 -0300

    Add dashboard page

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -8,9 +8,9 @@ const Dashboard = async ({
 }) => {
   const { page } = await getDictionary(lang);
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-3xl font-bold'>{page.about.title}</h1>
-      <h2 className='text-3xl font-bold'>{page.about.description}</h2>
+    <div className='flex flex-col items-center justify-between p-24 no-scrollbar'>
+      <h1 className='text-3xl font-bold'>{page.home.title}</h1>
+      <h2 className='text-3xl font-bold'>{page.home.description}</h2>
     </div>
   );
 };
