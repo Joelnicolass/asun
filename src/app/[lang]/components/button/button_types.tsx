32f246219762fa5dba0ef94f93e9f3eff416b853
Albano <albanokainer@hotmail.com>
2023-10-08 05:14:54 -0300

    Add types for button component

---
+++ src/app/[lang]/components/button/button_types.tsx
@@ -0,0 +1,5 @@
+interface ButtonProps{
+    color: "primary" | "danger" | "default" | "secondary" | "success" | "warning" | undefined;
+    variant: "bordered" | "solid" | "light" | "flat" | "faded" | "shadow" | "ghost" | undefined;
+    className?: string;
+}
