4312d224aa375029af30a9de3991ec7946bdc55d
Albano <albanokainer@hotmail.com>
2023-10-08 05:13:48 -0300

    Update use Reveal component effect

--- src/app/[lang]/(screens)/customers/page.tsx
+++ src/app/[lang]/(screens)/customers/page.tsx
@@ -1,21 +1,23 @@
-import { Locale } from "@/i18n.config";
-import { getDictionary } from "@/lib/dictionary";
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '@/lib/dictionary';
+import { Reveal } from '../../components/reveal/reveal';
 
-const Customers =async ({
+const Customers = async ({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) => {
-  
   const { page } = await getDictionary(lang);
   return (
     <div className='flex flex-col items-center justify-between p-24 no-scrollbar'>
-    <h1 className='text-3xl font-bold'>{page.customers.title}</h1>
-    <h2 className='text-3xl font-bold'>{page.customers.description}</h2>
-  </div>
+      <Reveal slideColor={''}>
+        <h1 className='text-3xl font-bold'>{page.customers.title}</h1>
+      </Reveal>
+      <Reveal slideColor={''}>
+        <h2 className='text-3xl font-bold'>{page.customers.description}</h2>
+      </Reveal>
+    </div>
+  );
+};
 
-  )
-}
-
-
-export default Customers
+export default Customers;
