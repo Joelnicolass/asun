c83f96aec505e3fbaefe1ce8c065ed64e673fa52
Albano <albanokainer@hotmail.com>
2023-10-10 22:22:25 -0300

    Minor update reference to dictionary

--- src/app/[lang]/(screens)/dashboard/page.tsx
+++ src/app/[lang]/(screens)/dashboard/page.tsx
@@ -17,12 +17,12 @@ const Dashboard = async ({
     <div className='flex flex-col items-start pl-3 sm:pl-5 justify-between no-scrollbar'>
       <Reveal slideColor={''}>
         <h1 className='text-2xl text-black md:text-3xl py-2 text-start md:pl-10 w-full sm:max-w-lg'>
-          {page.home.title}
+          {page.dashboard.title}
         </h1>
       </Reveal>
       <Reveal slideColor={''}>
         <AppButton color='success' variant='bordered'>
-          {page.home.title_button}
+          {page.dashboard.title_button}
         </AppButton>
       </Reveal>
     </div>
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -9,7 +9,7 @@ export default async function Home({
 
   return (
     <div className="flex flex-col items-center justify-between p-24">
-      <h1 className='text-3xl font-bold text-white'>{page.home.title}</h1>
+      <h1 className='text-3xl font-bold text-white'>{page.dashboard.title}</h1>
     </div>
   );
 }
