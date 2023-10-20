2925dc4f0649445cb620a529e2a4cfa9831d2fd6
joelsartori <joelnicolass@gmail.com>
2023-10-19 23:20:26 -0300

    vcard

--- src/app/vcard/page.tsx
+++ src/app/vcard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import "../[lang]/globals.css";
 
 import { AsunLogo } from "../[lang]/components/asun_logo/asun_logo";
 
@@ -11,53 +12,66 @@ const Products = () => {
         display: "flex",
         justifyContent: "center",
         flexDirection: "column",
-        alignItems: "center",
-        height: "100svh",
+        height: "100dvh",
         padding: "10px",
-        border: "1px solid #ccc",
-        borderRadius: "15px",
-        backgroundColor: "#fff",
+        backgroundColor: "#000000",
       }}
     >
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: 0.5 }}
+      <div
         style={{
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
+          flexDirection: "column",
           alignItems: "center",
-          fontSize: "30px",
-          fontWeight: "bold",
-          color: "#000",
-        }}
-      >
-        <AsunLogo size={100} />
-        FRANCISCO E. MACHEO LORCA
-      </motion.div>
-
-      <motion.button
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: 1 }}
-        style={{
-          width: "100%",
-          padding: "10px",
+          height: "100dvh",
+          padding: "20px",
           border: "1px solid #ccc",
           borderRadius: "15px",
           backgroundColor: "#fff",
-          color: "#000",
-          fontSize: "20px",
-          fontWeight: "bold",
-          marginTop: "20px",
-        }}
-        onClick={() => {
-          window.open("https://contact.asun.ar/fmacheo");
         }}
       >
-        Download Contact
-      </motion.button>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "100%",
+            fontWeight: "bold",
+            color: "#000",
+          }}
+        >
+          <AsunLogo size={100} />
+          FRANCISCO E. MACHEO LORCA
+        </motion.div>
+
+        <motion.button
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+          style={{
+            width: "100%",
+            padding: "10px",
+            border: "1px solid #ccc",
+            borderRadius: "15px",
+            backgroundColor: "#ffffff",
+            color: "#000",
+            fontSize: "100%",
+            fontWeight: "bold",
+            marginTop: "20px",
+          }}
+          onClick={() => {
+            window.open(
+              "https://drive.google.com/uc?export=download&id=1rUYsiC5vAGU7AJGfEj2fJm-J22tFioa_"
+            );
+          }}
+        >
+          Download Contact
+        </motion.button>
+      </div>
     </div>
   );
 };
